70dc3e8a67fe565c293bf697755998acb9b485d7
Default User <laurence.a.maddox@gmail.com>
2017-10-05 05:15:20 -0500

    backup

--- src/libarray.c
+++ src/libarray.c
@@ -54,8 +54,11 @@ array_t *ez_alloc_array (size_t esz, size_t n) {
 
 	eszs[0] = sizeof (array_t);
 	eszs[1] = datasz  (esz, n);
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wstrict-aliasing"
    combined[0] = (void *restrict *restrict) &caq;
    combined[1] = (void *restrict *restrict) &data;
+	#pragma GCC diagnostic pop
 	error_check (mmalloc2 (combined, eszs,
 		eszs[0] + eszs[1], ARRSZ (eszs)) != 0)
 		return NULL;
